1d388b369ad63d996b73f70dac26295a51d4d88a
hoangmanhcuong97 <hoangmanhcuong01997@gmail.com>
2021-06-09 11:29:41 +0700

    dien tich hinh chu nhat

---
+++ toantu.js
@@ -0,0 +1,8 @@
+function tinhtoan(){
+ let chieurong = prompt("Nhap do dai chieu rong");
+ let chieudai = prompt("Nhap do dai chieu dai");
+  let a = parseInt(chieurong);
+  let b = parseInt(chieudai);
+  let are = a * b;
+  document.write("Dien tich hcn = "+ are);
+}
